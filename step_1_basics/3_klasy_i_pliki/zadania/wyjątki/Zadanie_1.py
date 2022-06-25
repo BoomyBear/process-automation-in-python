3ece0362a9cf3e176a470e7ea2fbb278becc7a2e
Tomasz Szczygielski <tszczygielski@sii.pl>
2022-06-25 16:40:16 +0200

    EOD 1st day of training

--- step_1_basics/3_klasy_i_pliki/zadania/wyjątki/Zadanie_1.py
+++ step_1_basics/3_klasy_i_pliki/zadania/wyjątki/Zadanie_1.py
@@ -3,3 +3,17 @@ Pobierz dane numeryczne od użytkownika i zapisz je do listy.
 Użyj obsługi wyjątków by zwrócić użytkownikowi uwagę, gdy poda dane nienumeryczne.
 Kontynuuj pytanie o dane dopóki użytkownik nie wpisze litery "N"
 """
+
+numbers = []
+while True:
+    inputData = input('podaj liczbe (lub N by zakonczyc program): ')
+    if inputData == 'N':
+        print('Do zobaczenia.')
+        break
+    try:
+        numbers.append(int(inputData))
+    except ValueError:
+        print('Prosze podac liczbe')
+
+
+print(numbers)
