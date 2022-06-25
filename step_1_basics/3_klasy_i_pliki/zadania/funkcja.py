8318edc61cfa5017e541b983c35470833800d46d
Tomasz Szczygielski <tszczygielski@sii.pl>
2022-06-25 16:40:16 +0200

    EOD 1st day of training

--- step_1_basics/3_klasy_i_pliki/zadania/funkcja.py
+++ step_1_basics/3_klasy_i_pliki/zadania/funkcja.py
@@ -5,11 +5,15 @@ Hint: Palindrom to słowo brzmiące od przodu jak i od tyłu tak samo
 
 
 def is_palindrome(text):
-    pass
+    text = "".join(text.lower().split())
+    return text == text[::-1]
+
 
 
 assert is_palindrome("ala") == True
 assert is_palindrome("Oka") == False
 assert is_palindrome("OkO") == True
 assert is_palindrome("A to idiOta") == True
-assert is_palindrome("A to idiota!") == True
+#assert is_palindrome("A to idiota!") == True
+
+
--- step_1_basics/3_klasy_i_pliki/zadania/pliki/przyklad.py
+++ step_1_basics/3_klasy_i_pliki/zadania/pliki/przyklad.py
@@ -1,6 +1,16 @@
 from faker import Faker
 
 fake = Faker()
-with open("plik.txt", "w" ) as f:
+with open("plik.txt", "w" ) as f: # w - utworzy/wyczysci plik / zapisze dane
     lines = '\n'.join([fake.text() for _ in range(100)])
     f.writelines(lines)
+
+#
+# with open("plik.txt", "a" ) as f: # a - otworzy plik i doda na samym koncu
+#     lines = '\n'.join([fake.text() for _ in range(100)])
+#     f.writelines(lines)
+#
+#
+# with open("plik.txt", "r" ) as f: # r - odczytuje plik
+#     lines = '\n'.join([fake.text() for _ in range(100)])
+#     f.writelines(lines)
